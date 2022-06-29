c8be3a3bfd939518a7dccc3b00988043ec4da310
sim-ware <samir.gossain@gmail.com>
2022-06-29 14:17:03 +0100

    Adds ability to call API from within plugin code

--- plugin/code.ts
+++ plugin/code.ts
@@ -1,21 +1,43 @@
-for (const node of figma.currentPage.selection) {
-  if ("paddingBottom" in node && node.id === "small") {
-   console.log('paddingBottom present')
-    node.paddingBottom += 10
-  }
-  if ("paddingLeft" in node) {
-   console.log('paddingLeft present')
-    node.paddingLeft += 5
-  }
-  if ("paddingRight" in node) {
-   console.log('paddingRight present')
-    node.paddingRight += 10
-  }
-  if ("paddingTop" in node) {
-   console.log('paddingTop present')
-    node.paddingTop += 5
-  }
-  console.log('node', node)
-}
+figma.showUI(`
+  <script>
+    window.onmessage = async (event) => {
+      if (event.data.pluginMessage.type === 'networkRequest') {
+        var request = new XMLHttpRequest()
+        request.open('GET', 'https://api.sampleapis.com/countries/countries')
+        request.responseType = 'text'
+        request.onload = () => {
+          window.parent.postMessage({pluginMessage: request.response}, '*')
+        };
+        request.send()
+      }
+    }
+  </script>`, { visible: false }
+);
 
-figma.closePlugin();
+figma.ui.postMessage({ type: 'networkRequest' })
+
+figma.ui.onmessage = async (msg) => {
+  console.log('msg', msg)
+
+  for (const node of figma.currentPage.selection) {
+    if ("paddingBottom" in node) {
+    console.log('paddingBottom present')
+      node.paddingBottom += 5
+    }
+    if ("paddingLeft" in node) {
+    console.log('paddingLeft present')
+      node.paddingLeft += 5
+    }
+    if ("paddingRight" in node) {
+    console.log('paddingRight present')
+      node.paddingRight += 5
+    }
+    if ("paddingTop" in node) {
+    console.log('paddingTop present')
+      node.paddingTop += 5
+    }
+    console.log('node', node)
+  }
+
+  figma.closePlugin()
+}
