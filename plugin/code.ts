442021c680170c5de2a1a8499d003c75c6f5d0aa
sim-ware <samir.gossain@gmail.com>
2022-07-13 11:25:46 +0100

    Formats logging

--- plugin/code.ts
+++ plugin/code.ts
@@ -19,32 +19,34 @@ figma.ui.postMessage({ type: 'networkRequest' })
 figma.ui.onmessage = async (msg) => {
   const parsedJson = JSON.parse(msg)
   const { paddingTop, paddingBottom, paddingLeft, paddingRight } = parsedJson
-  // 
+
   console.log('parsedJson:',    parsedJson)
-  // 
   console.log('paddingTop:',    paddingTop)
   console.log('paddingBottom:', paddingBottom)
   console.log('paddingLeft:',   paddingLeft)
   console.log('paddingRight:',  paddingRight)
-  // //
 
   for (const node of figma.currentPage.selection) {
     if ("paddingBottom" in node) {
     console.log('paddingBottom present')
       node.paddingBottom = parseInt(paddingBottom)
     }
+
     if ("paddingLeft" in node) {
     console.log('paddingLeft present')
       node.paddingLeft = parseInt(paddingLeft)
     }
+
     if ("paddingRight" in node) {
     console.log('paddingRight present')
       node.paddingRight = parseInt(paddingRight)
     }
+
     if ("paddingTop" in node) {
     console.log('paddingTop present')
       node.paddingTop = parseInt(paddingTop)
     }
+    
     console.log('node', node)
   }
 
