a539c694d83c8011aaa2a6e60e009bca3560be98
sim-ware <samir.gossain@gmail.com>
2022-07-25 11:26:48 +0100

    Captures Frame Name and logs to console

--- plugin/code.ts
+++ plugin/code.ts
@@ -20,34 +20,36 @@ figma.ui.onmessage = async (msg) => {
   const parsedJson = JSON.parse(msg)
   const { paddingTop, paddingBottom, paddingLeft, paddingRight } = parsedJson
 
-  console.log('parsedJson:',    parsedJson)
-  console.log('paddingTop:',    paddingTop)
-  console.log('paddingBottom:', paddingBottom)
-  console.log('paddingLeft:',   paddingLeft)
-  console.log('paddingRight:',  paddingRight)
+  // console.log('parsedJson:',    parsedJson)
+  // console.log('paddingTop:',    paddingTop)
+  // console.log('paddingBottom:', paddingBottom)
+  // console.log('paddingLeft:',   paddingLeft)
+  // console.log('paddingRight:',  paddingRight)
 
   for (const node of figma.currentPage.selection) {
     if ("paddingBottom" in node) {
-    console.log('paddingBottom present')
+    // console.log('paddingBottom present')
       node.paddingBottom = parseInt(paddingBottom)
     }
 
     if ("paddingLeft" in node) {
-    console.log('paddingLeft present')
+    // console.log('paddingLeft present')
       node.paddingLeft = parseInt(paddingLeft)
     }
 
     if ("paddingRight" in node) {
-    console.log('paddingRight present')
+    // console.log('paddingRight present')
       node.paddingRight = parseInt(paddingRight)
     }
 
     if ("paddingTop" in node) {
-    console.log('paddingTop present')
+    // console.log('paddingTop present')
       node.paddingTop = parseInt(paddingTop)
     }
     
     console.log('node', node)
+    console.log('node.name', node.name)
+    console.log('node.id', node.id)
   }
 
   figma.closePlugin()
