5a1eedfb2d0eb7299a3494243506eda53aee617f
sim-ware <samir.gossain@gmail.com>
2022-08-02 17:39:05 +0100

    Validation complete for padding top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,33 +7,10 @@ import BrushIcon from '@mui/icons-material/Brush';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-// extract into firestore: 'src/services/firestore'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { doc, updateDoc } from "firebase/firestore"; 
-import { getFirestore } from 'firebase/firestore/lite';
-//
+import { initializeFirebase } from './Firestore';
 
-const firebaseConfig = {
-  // apiKey: "",
-  // authDomain: "design-system-prototype-5a79b.firebaseapp.com",
-  // projectId: "design-system-prototype-5a79b",
-  // storageBucket: "design-system-prototype-5a79b.appspot.com",
-  // messagingSenderId: "",
-  // appId: "",
-  // measurementId: ""
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
-
-
-const appStyle = {
-  '& > :not(style)': { m: 1, width: '25ch' },
-  maxWidth: '53vh'
-}
+const db = initializeFirebase();
 
 function App() {
   const [variableSet,   setVariableSet]   = useState(false);
@@ -77,10 +54,52 @@ function App() {
     return true;
   };
 
-  const checkForPaddingError = (paddingValue: string) => {
+  const checkForPaddingError = (paddingValue: string, position: string) => {
+    if (variableSet) {
+      if (position === 'top') {
+        const doesPaddingFormulaIncludeVariable = paddingTop.includes(variableName);
+
+        const doesPaddingFormulaIncludeOperand = ['+', '-', '*', '/' ].some(element => {
+          if (paddingTop.includes(element)) return true;
+          return false;
+        });
+
+        const isMultipleValid = parseInt(
+          paddingTop.replace(variableName, '')
+            .replace('+', '').replace('-', '').replace('*', '').replace('/', '')
+        ); 
+        
+        if (!doesPaddingFormulaIncludeVariable) return true;
+        if (!doesPaddingFormulaIncludeOperand)  return true;
+        if (!isMultipleValid)                   return true;
+      }
+    };
+
     if (!variableSet) return checkForOnlyNumericalCharacters(paddingValue);
+    
     return false;
   };
+
+  const getPaddingErrorHelperText = () => {
+    if (!variableSet) return checkForPaddingError(paddingTop, 'top') ? "Please use only numerical characters" : ""
+
+    if (variableSet) {
+      const doesPaddingFormulaIncludeOperand = ['+', '-', '*', '/' ].some(element => {
+        if (paddingTop.includes(element)) return true;
+        return false;
+      });
+
+      const isMultipleValid = parseInt(
+        paddingTop.replace(variableName, '')
+          .replace('+', '').replace('-', '').replace('*', '').replace('/', '')
+      ); 
+
+      if (!variableName)                      return "Please enter a variable name"
+      if (!paddingTop.includes(variableName)) return "Please include the variable in the formula"
+      if (!doesPaddingFormulaIncludeOperand)  return "Please include an operand in the formula"
+      if (!isMultipleValid)                   return "Please include a valid numerical value"
+    }
+  }
 
   const inactivateSubmitButton = () => {
     if (paddingTop === '' || paddingBottom === '' || paddingLeft === '' || paddingRight === '') return true;
@@ -95,8 +114,12 @@ function App() {
 
     console.log('variableName', variableName)
     console.log('variableValue', variableValue)
-
-    // what if padding values are numerical, but there is variable name?
+    
+    // if variable set, calculate the values here
+    // remove variable
+    // remove whitespace
+    // check order of operations 
+    // apply order of operations
 
     try {
       const styleRef = doc(db, "users", "6MU0LKOQPpG2k9nAbfBk");
@@ -123,7 +146,7 @@ function App() {
 
         <Box
           component="form"
-          sx={appStyle}
+          sx={{'& > :not(style)': { m: 1, width: '25ch' }, maxWidth: '53vh' }}
           noValidate
           autoComplete="off"
         >
@@ -156,32 +179,32 @@ function App() {
             id="outlined-basic"
             variant="outlined"
             label="Padding Top"
-            error={checkForPaddingError(paddingTop)}
-            helperText={checkForPaddingError(paddingTop) ? "Please use only numerical characters" : ""}
+            error={checkForPaddingError(paddingTop, 'top')}
+            helperText={getPaddingErrorHelperText()}
             onChange={e => onPaddingChange(e.target.value, paddingTop, 'top')}
           />
           <TextField
             id="outlined-basic"
             variant="outlined"
             label="Padding Bottom"
-            error={checkForPaddingError(paddingBottom)}
-            helperText={checkForPaddingError(paddingBottom) ? "Please use only numerical characters" : ""}
+            error={checkForPaddingError(paddingBottom, 'bottom')}
+            helperText={checkForPaddingError(paddingBottom, 'bottom') ? "Please use only numerical characters" : ""}
             onChange={e => onPaddingChange(e.target.value, paddingBottom, 'bottom')}
           />
           <TextField
             id="outlined-basic"
             variant="outlined"
             label="Padding Left" 
-            error={checkForPaddingError(paddingLeft)}
-            helperText={checkForPaddingError(paddingLeft) ? "Please use only numerical characters" : ""}
+            error={checkForPaddingError(paddingLeft, 'left')}
+            helperText={checkForPaddingError(paddingLeft, 'left') ? "Please use only numerical characters" : ""}
             onChange={e => onPaddingChange(e.target.value, paddingLeft, 'left')}
           />
           <TextField
             id="outlined-basic"
             variant="outlined"
             label="Padding Right"
-            error={checkForPaddingError(paddingRight)}
-            helperText={checkForPaddingError(paddingRight) ? "Please use only numerical characters" : ""}
+            error={checkForPaddingError(paddingRight, 'right')}
+            helperText={checkForPaddingError(paddingRight, 'right') ? "Please use only numerical characters" : ""}
             onChange={e => onPaddingChange(e.target.value, paddingRight, 'right')}
           />
 
---
+++ frontend/src/Firestore.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    // apiKey: "",
+    // authDomain: "design-system-prototype-5a79b.firebaseapp.com",
+    // projectId: "design-system-prototype-5a79b",
+    // storageBucket: "design-system-prototype-5a79b.appspot.com",
+    // messagingSenderId: "",
+    // appId: "",
+    // measurementId: ""
+};
+
+export const initializeFirebase = () => {
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+    const db = getFirestore();
+
+    return db;
+}
